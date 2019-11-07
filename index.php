7c17b01eae851f92b68833d5c56539a526a7bfd7
Yamada <57486435+alphadigital868@users.noreply.github.com>
2019-11-07 14:16:16 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,119 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>ial360&#176;</title>
+    <meta name="description" content="Plataforma ial360 - Instituto Alpha Lumen">
+    <meta name="author" content="daniel yamada">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="robots" content="all,follow">
+
+    <!-- Bootstrap CSS-->
+    <link rel="stylesheet" href="publico/vendor/bootstrap/css/bootstrap.min.css">
+    <!-- Font Awesome CSS-->
+    <link rel="stylesheet" href="publico/vendor/font-awesome/css/font-awesome.min.css">
+    <!-- Fontastic Custom icon font-->
+    <link rel="stylesheet" href="publico/css/fontastic.css">
+    <!-- Google fonts - Roboto -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700">
+    <!-- Bootstrap Select CSS-->
+    <link rel="stylesheet" href="publico/vendor/bootstrap-select/css/bootstrap-select.min.css">
+    <!-- Bootstrap Touchspin CSS-->
+    <link rel="stylesheet" href="publico/vendor/bootstrap-touchspin/jquery.bootstrap-touchspin.min.css">
+    <!-- Bootstrap Datepicker CSS-->
+    <link rel="stylesheet" href="publico/vendor/bootstrap-datepicker/css/bootstrap-datepicker3.css">
+
+    <!-- DataTables CSS-->
+    <link rel="stylesheet" href="publico/vendor/datatables/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="publico/vendor/datatables/css/buttons.bootstrap4.min.css">
+
+    <!-- theme stylesheet-->
+    <link rel="stylesheet" href="publico/css/style_ial.css" id="theme-stylesheet">
+    <!-- Custom stylesheet - for your changes-->
+    <link rel="stylesheet" href="publico/css/custom.css">
+
+    <!-- Favicon-->
+    <link rel="apple-touch-icon" sizes="57x57" href="publico/img/projeto/ico/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="publico/img/projeto/ico/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="publico/img/projeto/ico/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="publico/img/projeto/ico/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="publico/img/projeto/ico/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="publico/img/projeto/ico/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="publico/img/projeto/ico/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="publico/img/projeto/ico/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="publico/img/projeto/ico/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="publico/img/projeto/ico/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="publico/img/projeto/ico/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="publico/img/projeto/ico/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="publico/img/projeto/ico/favicon-16x16.png">
+    <link rel="manifest" href="publico/img/projeto/ico/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="publico/img/projeto/ico/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+
+  </head>
+<body >
+
+
+<form method="post" class="text-center form-validate" >
+    <div class="page login-page" id="background">
+        <div class="container " >
+            <div class="form-outer d-flex align-items-center">
+
+                <div class="form-inner dashboard-counts" style = "width:100%" >
+
+                    <div class="logo text-uppercase">
+                        <img src="publico/img/projeto/ial360-redo-30.png" alt="ial360" class="img-fluid pb-3" width="200" height="100">
+                        <!--    <strong class="text-primary">Plataforma ial360&#176;</strong> -->
+                    </div>
+
+
+                    <!-- Acesso Aluno -->
+                    <div class="col-12 pt-2 mb-4 ">
+                        <div class=" count-title d-flex">
+                            <div class="col-sm-12">
+                                <a class="row" href="app/login.php">
+                                    <div class="icon ml-2"><i class="fa fa-user" aria-hidden="true"></i></div>
+                                    <div class="name ml-1"><strong class="text-uppercase"> Login para quem já tem cadastro</strong></div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Acesso Escola -->
+                    <div class="col-12 mb-4 ">
+                        <div class=" count-title d-flex ">
+                            <div class="col-sm-12 ">
+                                <a class="row" href="app/register.php">
+                                    <div class="icon ml-2"><i class="fa fa-file-text-o" aria-hidden="true"></i></div>
+                                    <div class="name ml-1"><strong class="text-uppercase"> Criar Cadastro (caso não tenha)</strong></div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                 </div>
+
+                <div class="copyrights text-center">
+                    <p>&copy; 2017-2019 - Instituto Alpha Lumen - Plataforma ial360&#176; - designed by <span class="text-success">Alpha Digital</span></p>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</form>
+
+    <!-- JavaScript files-->
+    <script src="publico/vendor/jquery/jquery.min.js"></script>
+    <script src="publico/vendor/popper.js/umd/popper.min.js"> </script>
+    <script src="publico/vendor/bootstrap/js/bootstrap.min.js"></script>
+    <script src="publico/js/grasp_mobile_progress_circle-1.0.0.min.js"></script>
+    <script src="publico/vendor/jquery.cookie/jquery.cookie.js"> </script>
+    <script src="publico/vendor/chart.js/Chart.min.js"></script>
+    <script src="publico/vendor/jquery-validation/jquery.validate.min.js"></script>
+
+ 
+
+</body>
+</html>
